2593ef28e43ba6034c589cd3165fc4c51720a5f8
himansi-choudhary <himansisolanki28@gmail.com>
2026-06-27 15:25:53 +0530

    Nav Updated

--- js/sidemenu.js
+++ js/sidemenu.js
@@ -1,6 +1,20 @@
 const sidemenu = document.getElementById('sidemenu');
 const menuOpen = document.getElementById('menuOpen');
 const menuClose = document.getElementById('menuClose');
+const navbar = document.querySelector('nav');
+
+const updateNavbar = () => {
+    if (!navbar) return;
+
+    if (window.scrollY > 50) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+};
+
+window.addEventListener('scroll', updateNavbar);
+window.addEventListener('load', updateNavbar);
 
 // Open side menu
 menuOpen.addEventListener('click', () => {
